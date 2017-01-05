335c17d19e2994936881de248a1247b9c039dc20
FrankieFabuloso <frocha@ucsc.edu>
2017-01-05 10:20:01 -0800

    adds todo delete spec functionality

--- to-do-list/src/components/App.js
+++ to-do-list/src/components/App.js
@@ -1,29 +1,24 @@
 import React, { Component } from 'react';
 import logo from '../images/logo.svg';
 import '../stylesheets/App.css';
-import Todo from './Todo'
 import List from './List'
+import Textbox from './Textbox'
 
 class App extends React.Component {
   render() {
     return (
       <div className='App'>
         <table className='table'>
-        <thead>
-          <tr>
-            <td></td>
-            <td className='header'>Tasks</td>
-            <td></td>
-          </tr>
-        </thead>
-        <List />
+          <thead>
+            <tr>
+              <td></td>
+              <td className='header'>Tasks</td>
+              <td></td>
+            </tr>
+          </thead>
+          <List />
         </table>
-        <div className="input-group col-lg-6">
-          <span className="input-group-btn">
-            <button className="btn btn-secondary" type="button">Go!</button>
-          </span>
-          <input type="text" className="form-control" placeholder="What do you want to do?"></input>
-        </div>
+        <Textbox />
       </div>
     );
   }
--- to-do-list/src/components/List.js
+++ to-do-list/src/components/List.js
@@ -1,34 +1,38 @@
 import React from 'react';
 import Todo from './Todo'
 
-// temporary static data
-const tasks = [
-  {id:1, task: "Dance a lot"},
-  {id:2, task: "Run a mile"},
-  {id:3, task: "Drink some soda"},
-  {id:4, task: "Task #4"},
-  {id:5, task: "Computers"},
-  {id:6, task: "Code"}
-]
-
-const TodoList = (props) => {
-  const todos = props.tasks
-  return (
-    <tbody>
-      { todos.map((todo) =>
-        <Todo key={todo.id} id={todo.id} value={todo.task} />
-      )}
-    </tbody>
-  );
-}
-
 class List extends React.Component{
   constructor(props) {
     super(props)
+    this.state = {
+      Tasks : [
+        {id:1, task: "Dance a lot"},
+        {id:2, task: "Run a mile"},
+        {id:3, task: "Drink some soda"},
+        {id:4, task: "Task #4"},
+        {id:5, task: "Computers"}
+      ]
+    }
+    this.handleDeleteClick.bind(this)
   }
+
+  handleDeleteClick(e, todo) {
+    const newState = this.state.Tasks
+    const todoIndex = newState.indexOf(todo)
+    newState.splice(todoIndex,1)
+    this.setState({
+      Tasks: newState
+    })
+  }
+
   render() {
+    const todos = this.state.Tasks
     return(
-      <TodoList tasks={tasks} />
+      <tbody>
+        { todos.map((todo) =>
+          <Todo key={todo.id} id={todo.id} value={todo.task} onClick={ (e) => this.handleDeleteClick(e, todo) } />
+        )}
+      </tbody>
     )
   }
 }
